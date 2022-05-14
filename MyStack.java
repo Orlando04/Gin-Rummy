b943825a452b1412de73fb4a4ed300912542474c
Orlando04 <orlando.medina4@upr.edu>
2022-05-14 12:02:35 -0400

    added comments on MyStack

--- MyStack.java
+++ MyStack.java
@@ -1,3 +1,15 @@
+// Name: Orlando Medina Rodríguez
+// Student #: 801-17-7833
+// CCOM4029-002
+// Prof. Patricia Ordonez
+
+// file name: MyStack.java
+
+/*  The purpose of MyStack is to create a stack using primitive data types,
+    in my case we are using arrays, to travel the deck of cards a player 
+    has in order to make the game of Rummy to flow better. This file contains 
+    methods for removing, adding and viewing elements in the players hand 
+*/
 
 public class MyStack {
 
@@ -12,7 +24,7 @@ public class MyStack {
         length = 0;
     }
 
-        // Pushing element on the top of the stack
+        // This method will push elements on top of the stack
         public void stack_push(Card arr[], Card val, int pos){
 
             if (top + 1 == 52)
@@ -21,7 +33,7 @@ public class MyStack {
             else arr[pos] = val;
         }
         
-        // Popping element from the top of the stack
+        // This method will remove elements on top of the stack
         public void stack_pop(){
 
             if (top == -1)
@@ -30,7 +42,8 @@ public class MyStack {
             else top--;
             
         }
-        // Displaying element on the top of the stack
+
+        // This will show the all the elements in the stack
         public Card stack_peek(){
 
             if (top == -1) return null;
